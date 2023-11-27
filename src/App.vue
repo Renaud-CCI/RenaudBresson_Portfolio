6ebba3c4321edc7e4363252c47194c81201e0a79
Renaud Bresson <renaud.cci@gmail.com>
2023-11-27 14:17:15 +0100

    create/Navbar-before-install-i18

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
     <v-card>
-    <v-toolbar color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar :color="secondaryColor">
 
-      <v-toolbar-title>Your Dashboard</v-toolbar-title>
+      <v-toolbar-title :style="{ color: primaryColor }">Your Dashboard</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
+      <v-btn icon  :color="primaryColor">
+        <v-icon :color="primaryColor">mdi-cog</v-icon>
       </v-btn>
 
       <template v-slot:extension>
         <v-tabs
+          :style="{ color: lightenPrimaryColor }"
+          :color="primaryColor"
+          :bg-color="secondaryColor"
+          :slider-color="primaryColor"
           v-model="tab"
           align-tabs="title"
         >
@@ -46,6 +45,9 @@
 </template>
 
 <script>
+import { onMounted } from 'vue';
+import { store } from './stores/store';
+
   export default {
     data () {
       return {
@@ -53,8 +55,45 @@
         items: [
           'web', 'shopping', 'videos', 'images', 'news',
         ],
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
       }
     },
+    computed: {
+      primaryColor() {
+        return store.primaryColor;
+      },
+      secondaryColor() {
+        return store.secondaryColor;
+      },
+      lightenPrimaryColor() {
+        return this.getLightenColor(store.primaryColor, 80);
+      }
+    },
+    mounted() {
+        console.log(store);
+        document.documentElement.style.setProperty('--primary-color', store.primaryColor);
+        document.documentElement.style.setProperty('--secondary-color', store.secondaryColor);
+    },
+    methods: {
+      /**
+       * Returns a lightened version of the given color.
+       * @param {string} color - The color to be lightened.
+       * @param {number} amount - The amount by which to lighten the color.
+       * @returns {string} - The lightened color.
+       */
+      getLightenColor(color, amount) {
+      const num = parseInt(color.replace('#', ''), 16); // convert color to a 32bit-integer
+      let r = (num >> 16) + amount;
+      r > 255 && (r = 255);
+      r < 0 && (r = 0);
+      let g = (num & 0x0000ff) + amount;
+      g > 255 && (g = 255);
+      g < 0 && (g = 0);
+      let b = ((num >> 8) & 0x00ff) + amount;
+      b > 255 && (b = 255);
+      b < 0 && (b = 0);
+      return `#${(g | (b << 8) | (r << 16)).toString(16)}`;
+      }
+    }
   }
 </script>
---
+++ src/stores/store.ts
@@ -0,0 +1,6 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+  primaryColor: '#425b8a',
+  secondaryColor: '#fae3ad',
+});
