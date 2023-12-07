04733153bfe7c06ca976e85342eb3239f667e00a
Renaud Bresson <renaud.cci@gmail.com>
2023-12-07 16:20:40 +0100

    fix/Contacts-responsive

--- src/components/contacts/ContactsContainer.vue
+++ src/components/contacts/ContactsContainer.vue
@@ -9,7 +9,7 @@
     }"
   >
 
-    <div class="p-4">
+    <div class="form-container">
       <form 
       id="fs-frm" 
       name="simple-contact-form" 
@@ -18,15 +18,14 @@
       method="post"
       class="flex flex-col justify-center items-center">
 
-        <h2 class="text-3xl font-bold m-6 px-64 pt-4 text-center">
+        <h2 class="title1 font-bold text-center">
           {{ translations.title.part1 }}
           {{ translations.title.part2 }}
           {{ translations.title.part3 }}
         </h2>
 
-        <div class="inline-flex my-6">
+        <div class="link-div inline-flex">
           <a 
-            class="mx-8" 
             href="https://www.linkedin.com/in/renaud-bresson" 
             :title="translations['linked-in-tag']" 
             target="_blank"
@@ -35,7 +34,6 @@
           </a>
 
           <a 
-            class="mx-8" 
             href="https://github.com/Renaud-CCI" 
             target="_blank" 
             :title="translations['github-tag']"
@@ -45,28 +43,28 @@
         </div>
 
 
-        <h2 class="text-3xl font-bold m-6">{{ translations.subtitle }}</h2>
+        <h2 class="title2 font-bold text-center">{{ translations.subtitle }}</h2>
 
-        <fieldset id="fs-frm-inputs" class="grid grid-cols-2 mt-6">
+        <fieldset id="fs-frm-inputs" class="contact-div grid grid-cols-2">
 
-          <div class="col-span-1 grid grid-cols-2 mx-20">
-            <label class="col-span-2 text-xl pb-2" for="full-name">{{ translations.name }}</label>
-            <input class="col-span-2 bg-white p-1 w-full h-10 rounded text-lg mb-8" type="text" name="name" id="full-name" :placeholder=" translations['name-placeholder']" required="">
+          <div class="input-div col-span-2 sm:col-span-1 grid grid-cols-2">
+            <label class="col-span-2" for="full-name">{{ translations.name }}</label>
+            <input class="col-span-2 bg-white p-1 w-full h-10 rounded" type="text" name="name" id="full-name" :placeholder=" translations['name-placeholder']" required="">
           </div>
 
-          <div class="col-span-1 grid grid-cols-2 mx-20">
-            <label class="col-span-2 text-xl pb-2" for="email-address">{{ translations.email }}</label>
-            <input class="col-span-2 bg-white p-1 w-full h-10 rounded text-lg mb-8" type="email" name="_replyto" id="email-address" :placeholder=" translations['email-placeholder']" required="">
+          <div class="input-div col-span-2 sm:col-span-1 grid grid-cols-2">
+            <label class="col-span-2" for="email-address">{{ translations.email }}</label>
+            <input class="col-span-2 bg-white p-1 w-full h-10 rounded" type="email" name="_replyto" id="email-address" :placeholder=" translations['email-placeholder']" required="">
           </div>
 
-          <div class="col-span-2 grid grid-cols-2 mx-20">
-            <label class="col-span-2 text-xl pb-2" for="message"> {{ translations.message }}</label>
-            <textarea class="col-span-2 bg-white p-1 w-full rounded text-lg mb-8" rows="5" name="message" id="message" :placeholder=" translations['message-placeholder']" required=""></textarea>
+          <div class="input-div col-span-2 grid grid-cols-2">
+            <label class="col-span-2" for="message"> {{ translations.message }}</label>
+            <textarea class="col-span-2 bg-white p-1 w-full rounded" rows="5" name="message" id="message" :placeholder=" translations['message-placeholder']" required=""></textarea>
           </div>
           
           <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
         </fieldset>
-        <input class='text-xl rounded px-4 py-3' type="submit" :value="translations['submit']" :style="{ 
+        <input class='submit-button rounded' type="submit" :value="translations['submit']" :style="{ 
       color: secondaryColor, 
       backgroundColor: primaryColor,
       boxShadow: '0 0 3px ' + primaryColor,
@@ -121,12 +119,135 @@ export default {
 .contacts-container{
   min-height:80vh;
 
-  .v-icon{
-    font-size: 6rem;
-    transition: color 0.2s ease-in-out;
+  .form-container {
 
-    &:hover {
-      color: var(--light-color);
+    .title1 {
+      padding: 2rem 20rem 0;
+      font-size: 2.2rem;
+    }
+
+    .link-div {
+      a {
+        font-size: 6rem;
+        margin: 0 3rem;
+        transition: color 0.2s ease-in-out;
+
+        &:hover {
+          color: var(--light-color);
+        }
+      }
+    }
+
+    .title2 {
+      padding: 1rem 20rem;
+      font-size: 2.2rem;
+    }
+
+    .contact-div {
+      padding: 2rem 20rem;
+      font-size: 1.7rem;
+
+      .input-div {
+        padding: 0 3rem;
+        label {
+          padding: 0 0 0.5rem;
+        }
+        input {
+          margin-bottom: 2rem;
+        }
+      }
+    }
+
+    .submit-button {
+      margin: 2rem 0;
+      padding: 1rem 2rem;
+      font-size: 1.6rem;
+    }
+  }
+
+   @media screen and (max-width: 1535px) {
+    .form-container {
+
+      .title1 {
+        padding: 1rem 20rem 0;
+        font-size: 1.5rem;
+      }
+
+      .link-div {
+        a {
+          font-size: 4rem;
+          margin: 0 2rem;
+        }
+      }
+
+      .title2 {
+        padding: 0.8rem 20rem;
+        font-size: 1.5rem;
+      }
+
+      .contact-div {
+        padding: 0 20rem;
+        font-size: 1.2rem;
+
+        .input-div {
+          padding: 0 3rem;
+          label {
+            padding: 0 0 0.2rem;
+          }
+          input {
+            margin-bottom: 1rem;
+          }
+        }
+      }
+
+      .submit-button {
+        margin: 1rem 0;
+        padding:  0.5rem 1.5rem;
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+      .form-container {
+
+      .title1 {
+        padding: 0.5rem 1rem 0;
+        font-size: 1.1rem;
+      }
+
+      .link-div {
+        a {
+          font-size: 3rem;
+          margin: 0 1rem;
+        }
+      }
+
+      .title2 {
+        padding: 0.8rem 1rem;
+        font-size: 1.1rem;
+      }
+
+      .contact-div {
+        padding: 0 1rem;
+        font-size: 1rem;
+
+        .input-div {
+          padding: 0 3rem;
+          label {
+            padding: 0 0 0.2rem;
+          }
+          input {
+            margin-bottom: 1rem;
+          }
+        }
+      }
+
+      .submit-button {
+        margin: 1rem 0;
+        padding: 0.5rem 1rem;
+        font-size: 1rem;
+      }
     }
   }
 }
